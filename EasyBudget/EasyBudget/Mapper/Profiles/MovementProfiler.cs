8d45d5fd1705fb630e993e9166f8057ca694834c
Lucas Mariano dos Santos <lucasmsantos0153@gmail.com>
2023-05-08 19:39:21 -0300

    Criando profiler do automapper para Movements

---
+++ EasyBudget/EasyBudget/Mapper/Profiles/MovementProfiler.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EasyBudget.Data.Dto.MovementDto;
+using EasyBudget.Data.Models;
+
+namespace EasyBudget.Mapper.Profiles
+{
+    public class MovementProfiler : Profile
+    {
+        public MovementProfiler()
+        {
+            CreateMap<CreateMovementDto, Movement>();
+            CreateMap<Movement, ReadMovementDto>();
+            CreateMap<UpdateMovementDto, Movement>();
+        }
+    }
+}
