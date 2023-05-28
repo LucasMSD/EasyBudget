e1adb86e9b4c29d452df79c90b1f7ef05beb778f
Lucas Mariano dos Santos <lucasmsantos0153@gmail.com>
2023-05-28 13:07:22 -0300

    [change] alterando validação do campo Date sem transformar em DateOnly

--- EasyBudget/EasyBudget/Data/Dto/MovementDto/Validators/UpdateMovementDtoValidator.cs
+++ EasyBudget/EasyBudget/Data/Dto/MovementDto/Validators/UpdateMovementDtoValidator.cs
@@ -9,7 +9,7 @@ namespace EasyBudget.Data.Dto.MovementDto.Validators
             RuleFor(x => x.Id).NotEmpty().GreaterThan(0);
             RuleFor(x => x.Amount).NotEmpty().GreaterThan(0);
             RuleFor(x => x.Title).NotEmpty().MaximumLength(50);
-            RuleFor(x => DateOnly.FromDateTime(x.Date)).NotEmpty().LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Now));
+            RuleFor(x => x.Date.Date).NotEmpty().LessThanOrEqualTo(DateTime.Now.Date);
             RuleFor(x => x.Type).NotEmpty().IsInEnum();
             RuleFor(x => x.CategoryId).NotEmpty().GreaterThan(0);
             RuleFor(x => x.Description).MaximumLength(200);
